cb7252a787b28ae88c26a300eccb92f8673855f3
Richard <richardabrahams75@gmail.com>
2018-03-10 23:38:56 +0000

    nicer loader

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Loader } from "semantic-ui-react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default class Loading extends Component {
   render() {
     return (
-      <div>
-        <h1>LOADING</h1>
-      </div>
-    )
+      <Wrapper>
+        <Loader active size="massive" style={{ transform: "matrix(2,0,0,2,0,-64" }} />
+      </Wrapper>
+    );
   }
 }
