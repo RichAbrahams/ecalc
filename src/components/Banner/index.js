0aaac4d8d85ca34cb4e3f3dc3f01d39428ae0558
Richard <richardabrahams75@gmail.com>
2018-03-14 00:48:54 +0000

    logo

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import SiteName from './SiteName';
-import Nav from './nav';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import SiteName from "./SiteName";
+import Nav from "./nav";
+import styled from "styled-components";
 import * as actions from "../../actions";
-import Description from './Description';
+import Description from "./Description";
+import LogoBig from './LogoBig';
+import LogoSmall from './LogoSmall';
 
 const OuterWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  grid-row-gap: 3rem;
-  padding: 1rem 2rem 1rem 2rem;
-  background: rgba(33,143,208,1);
+  grid-row-gap: 1rem;
+  padding: 1rem 1rem 1rem 1rem;
+  background: rgba(33, 143, 208, 1);
   color: white;
+  @media screen and (min-width: 480px) {
+    grid-row-gap: 3rem;
+  }
   @media screen and (min-width: 700px) {
     grid-template-columns: 1fr;
   }
@@ -25,32 +30,30 @@ const Wrapper = styled.div`
   @media screen and (min-width: 1000px) {
     align-items: flex-start;
   }
-`
+`;
 
 export class Banner extends Component {
-
   resetPage(e) {
     this.props.resetRecipeState();
     this.props.setPage(1);
-  };
+  }
 
-render() {
-  return (
-    <OuterWrapper>
-      <Wrapper>
-      <SiteName>E-LIQUID WIZARD</SiteName>
-      <Description />
-      </Wrapper>
-      <Nav resetPage={(e) => this.resetPage(e)}/>
-    </OuterWrapper>
-  )
-}
+  render() {
+    return (
+      <OuterWrapper>
+        <Wrapper>
+        <LogoBig />
+        <LogoSmall />
+        </Wrapper>
+        <Nav resetPage={e => this.resetPage(e)} />
+      </OuterWrapper>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
   setPage: payload => dispatch(actions.setPage(payload)),
   resetRecipeState: () => dispatch(actions.resetRecipeState())
-
 });
 
 export default connect(null, mapDispatchToProps)(Banner);
